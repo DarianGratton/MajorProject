da6018bea9500fef9ccc6416611c031765a663cc
Darian Gratton <dariangratton@gmail.com>
2021-02-14 18:23:29 -0800

    Created Scene classes

--- Game/src/scene.h
+++ Game/src/scene.h
@@ -8,14 +8,14 @@
 
 using namespace entityx;
 
-class Scene : public EntityX {
+class Scene {
 public:
     Scene(std::string filename);
 
     void load(std::string filename);
-    inline std::string getSceneName() { return scenename; }
+    inline std::string getName() { return name; }
 
 private:
-    std::string scenename;
+    std::string name;
     std::string filename;
 };
--- Game/src/sceneManager.h
+++ Game/src/sceneManager.h
@@ -1,3 +1,20 @@
 #pragma once
 
 #include "Scene.h"
+
+class SceneManager {
+public:
+    static SceneManager& instance() {
+        static SceneManager *instance = new SceneManager();
+        return *instance;
+    }
+
+    SceneManager(SceneManager const&) = delete;
+    void operator=(SceneManager const&) = delete;
+
+    void addScene(std::string scenename, std::string filename);
+    void loadScene(std::string scenename);
+
+private:
+    SceneManager() {}
+};
