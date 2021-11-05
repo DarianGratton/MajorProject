a9adac8ee9b878dd7ee2429c740917d7ca363d08
Darian Gratton <dariangratton@gmail.com>
2021-11-04 22:25:00 -0700

    Add base implementation for ReplayMemory

--- Agent/Agent/Agent/Networks/SAC/ReplayMemory.cpp
+++ Agent/Agent/Agent/Networks/SAC/ReplayMemory.cpp
@@ -1,18 +1,64 @@
 #include "ReplayMemory.h"
 
-ReplayMemory::ReplayMemory()
-{
+#include <iostream>
+#include <random>
 
+ReplayMemory::ReplayMemory(unsigned int memSize, list<float> inputDims, unsigned int nActions) : memSize(memSize)
+{
+	if (memSize == 0)
+	{
+		cerr << "Error: memSize should not be 0, defaulting to 1" << endl;
+		memSize = 1;
+	}
 }
 
-void ReplayMemory::StoreStateTransition()
+void ReplayMemory::StoreStateTransition(
+	list<State> state,
+	list<unsigned int> action,
+	long reward,
+	list<State> newState,
+	bool terminal)
 {
+	// If memory is full then remove first element
+	if (stateMem.size() >= memSize)
+	{
+		stateMem.erase(stateMem.begin());
+		actionMem.erase(actionMem.begin());
+		rewardMem.erase(rewardMem.begin());
+		newStateMem.erase(newStateMem.begin());
+		terminalMem.erase(terminalMem.begin());
+	}
 
+	// Insert elements
+	stateMem.push_back(state);
+	actionMem.push_back(action);
+	rewardMem.push_back(reward);
+	newStateMem.push_back(newState);
+	terminalMem.push_back(terminal);
 }
 
-void ReplayMemory::SampleMemory()
+ReplayMemory::MemorySample ReplayMemory::SampleMemory(unsigned int batchSize)
 {
+	// Generate random sample
+	unsigned int memCount = static_cast<unsigned int>(stateMem.size());
+	std::random_device rd;
+	std::mt19937 gen(rd());
+	std::uniform_int_distribution<> distrib(0, memCount - 1);
 
+	// Get random sample
+	MemorySample sample;
+	for (int i = 0; i <= batchSize; ++i)
+	{
+		int batch = distrib(gen);
+		sample.states.push_back(stateMem.at(batch));
+		sample.newStates.push_back(newStateMem.at(batch));
+		sample.actions.push_back(actionMem.at(batch));
+		sample.rewards.push_back(rewardMem.at(batch));
+		sample.terminals.push_back(terminalMem.at(batch));
+	}
+
+	// Return object
+	return sample;
 }
 
 void ReplayMemory::SaveMemory()
--- Agent/Agent/Agent/Networks/SAC/ReplayMemory.h
+++ Agent/Agent/Agent/Networks/SAC/ReplayMemory.h
@@ -1,27 +1,51 @@
 #pragma once
 
+#include <list>
+#include <vector>
 #include <torch/torch.h>
+
+#include "../../Environment/Reward.h"
+#include "../../Environment/State.h"
+
+using namespace std;
 
 // TODO: Implement functionality for a basic SACAgent
 class ReplayMemory
 {
 public:
 
-	ReplayMemory();
-
-	void StoreStateTransition();
-	void SampleMemory();
-	
-	void SaveMemory();
-	void LoadMemory();
+	ReplayMemory(unsigned int memSize, list<float> inputDims, unsigned int nActions);
 
 private:
-	unsigned int maxMemSize;
-	unsigned int memCounter;
-	int stateMem;	 // Placeholder type
-	int newStateMem; // Placeholder type
-	int actionMem;	 // Placeholder type
-	int rewardMem;	 // Placeholder type
-	int terminalMem; // Placeholder type
+	unsigned int memSize;
+	
+	// Memory
+	vector<list<State>> stateMem;       
+	vector<list<State>> newStateMem;    
+	vector<list<unsigned int>> actionMem;
+	vector<long> rewardMem;					// Reward should be a list of reward objects
+	vector<bool> terminalMem;
 
+	// Struct
+	struct MemorySample
+	{
+		vector<list<State>> states;
+		vector<list<unsigned int>> actions;
+		vector<long> rewards;
+		vector<list<State>> newStates;
+		vector<bool> terminals;
+	};
+
+public:
+	void StoreStateTransition(
+		list<State> state,
+		list<unsigned int> action,
+		long reward,
+		list<State> newState,
+		bool terminal);
+
+	MemorySample SampleMemory(unsigned int batchSize);
+
+	void SaveMemory();
+	void LoadMemory();
 };
