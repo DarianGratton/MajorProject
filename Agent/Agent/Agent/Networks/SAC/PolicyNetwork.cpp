901537a7774f7f9af70eecb88c24766e14a60b44
Darian Gratton <dariangratton@gmail.com>
2021-11-08 20:35:31 -0800

    Implemented Constructor and Forward of the PolicyNetwork class

--- Agent/Agent/Agent/Networks/SAC/PolicyNetwork.cpp
+++ Agent/Agent/Agent/Networks/SAC/PolicyNetwork.cpp
@@ -1,14 +1,32 @@
 #include "PolicyNetwork.h"
 
-PolicyNetwork::PolicyNetwork()
+PolicyNetwork::PolicyNetwork(float lr, 
+	unsigned int nActions, unsigned int maxActions,
+	int64_t inputDims, int64_t layer1Dims, int64_t layer2Dims) :
+	learningRate(lr), numActions(nActions), maxNumActions(maxActions)
 {
-
+	// Set up network
+	layer1 = torch::nn::Linear(inputDims, layer1Dims);
+	layer2 = torch::nn::Linear(layer1Dims, layer2Dims);
+	mu = torch::nn::Linear(layer2Dims, numActions);
+	sigma = torch::nn::Linear(layer2Dims, numActions);
+	
+	optimizer = new torch::optim::Adam(parameters(), learningRate);
 }
 
-torch::Tensor PolicyNetwork::Forward()
+std::pair<torch::Tensor, torch::Tensor> PolicyNetwork::Forward(torch::Tensor state)
 {
-	torch::Tensor w;
-	return w;
+	torch::Tensor prob = layer1(state);
+	prob = torch::nn::functional::relu(prob);
+	prob = layer2(prob);
+	prob = torch::nn::functional::relu(prob);
+
+	torch::Tensor muOutput = mu(prob);
+	torch::Tensor sigmaOutput = sigma(prob);
+
+	sigmaOutput = torch::clamp(sigmaOutput, c10::Scalar(reparamNoise), c10::Scalar(1));
+
+	return std::make_pair(muOutput, sigmaOutput);
 }
 
 void PolicyNetwork::CalculateActionProb()
--- Agent/Agent/Agent/Networks/SAC/PolicyNetwork.h
+++ Agent/Agent/Agent/Networks/SAC/PolicyNetwork.h
@@ -7,16 +7,26 @@ class PolicyNetwork : torch::nn::Module
 {
 public:
 
-	PolicyNetwork();
+	PolicyNetwork(float lr, 
+		unsigned int nActions, unsigned int maxActions,
+		int64_t inputDims, int64_t layer1Dims, int64_t layer2Dims);
 
-	torch::Tensor Forward();
+	std::pair<torch::Tensor, torch::Tensor> Forward(torch::Tensor state);
 	void CalculateActionProb();
 	
 	void SaveMemory();
 	void LoadMemory();
 
 private:
+	const float reparamNoise = 1e-6;
 	float learningRate;
 	unsigned int maxNumActions;
 	unsigned int numActions;
+
+	// Network layers
+	torch::nn::Linear layer1 = nullptr;
+	torch::nn::Linear layer2 = nullptr;
+	torch::nn::Linear mu = nullptr;
+	torch::nn::Linear sigma = nullptr;
+	torch::optim::Adam* optimizer;
 };
